723aaecce9cbd23acd2fda0580a7dcbfe478c296
wzexn <635837931@qq.com>
2022-10-04 21:06:18 +0800

    search模块动态展示产品列表

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,6 @@
 //当前文件是对接口的统一管理
 import requests from "./request";
 import mockquests from "./mockAjax"
- import { get } from "core-js/core/dict"; 
 //三级联动的接口
 //http:gmall-h5-api.atguigu.cn
 
@@ -25,3 +24,21 @@ export const reqFloorList = ()=>
         url:'/floor',
         method:'get'
       })
+
+//获取搜索模块的数据 地址：/api/list 请求方式：post 需要带参数
+/*"category3Id": "61",
+  "categoryName": "手机",
+  "keyword": "小米",
+  "order": "1:desc",
+  "pageNo": 1,
+  "pageSize": 10,
+  "props": ["1:1700-2799:价格", "2:6.65-6.74英寸:屏幕尺寸"],
+  "trademark": "4:小米"*/
+
+  export const reqGetSearchInfo = (params)=>
+    requests({
+    url:"/list",
+    method:"post",
+    data:params,
+  });
+
--- src/store/search/index.js
+++ src/store/search/index.js
@@ -1,9 +1,40 @@
+import { reqGetSearchInfo } from "@/api";
 //search模块的小仓库
-const state = {};
-const mutations = {};
-const actions = {};
-const getters = {};
+const state = {
+    //仓库初始状态
+    searchList: {}
+};
+const mutations = {
+    GETSEARCHLIST(state, searchList) {
+        state.searchList = searchList;
+    },
+};
+const actions = {
+    //获取search模块的数据
+    async getSearchList({ commit }, params = {}) {
+        //当前这个reqGetSearchInfo在调用服务器数据的时候，至少需要传递一个参数（空对象）
+        //params形参：是当前用户派发actions的时候，第二个参数传递过来的，至少是一个空对象
+        let result = await reqGetSearchInfo(params);
+        if (result.code == 200) {       
+            commit("GETSEARCHLIST", result.data);
+        }
+    }
+};
+//计算属性
+//项目当中gettings主要的作用是  ：简化仓库中的数据
+//可以把我们将来要用的数据简化一下【方便获取数据】  
+const getters = {
+    goodsList(state){
+        return state.searchList.goodsList;
+    },
+    trademarkList(state){
+        return state.searchList.trademarkList;
+    },
+    attrsList(state){
+        return state.searchList.attrsList;
 
+    }, 
+};
 export default {
     state,
     mutations,
